6cf8d2fa942c9fab93697baccc4ffa12e0d3acf7
zhouliujun <1096432931@qq.com>
2022-08-20 18:18:41 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<!--
+ * @Author: zhouliujun
+ * @Date: 2022-08-19 09:41:50
+ * @LastEditTime: 2022-08-20 18:15:01
+ * @LastEditors: zhouliujun
+ * @Description: file content
+ * @FilePath: /vue-super-print/src/App.vue
+ * 可以输入预定的版权声明、个性签名、空行等
+-->
+<template>
+  <div class="app">
+    <!-- <visual-editor v-model="jsonData" :config="visualConfig" /> -->
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+// import { VisualEditor } from "../src/packages/visual-editor";
+// import { TestUseModel } from "../src/packages/utils/useModel";
+// import visualConfig from "./packages/visual.config";
+
+export default defineComponent({
+    name: "App",
+    // components: { VisualEditor },
+    setup() {
+        const jsonData = reactive({
+            container: {
+                height: 500,
+                width: 800
+            },
+            blocks: [
+                {
+                    componentKey: "button",
+                    top: 100,
+                    left: 100,
+                    focus: false,
+                    width: 0,
+                    height: 0,
+                    adjustPosition: true,
+                    props: {}
+                },
+                {
+                    componentKey: "input",
+                    top: 200,
+                    left: 200,
+                    focus: false,
+                    width: 0,
+                    height: 0,
+                    adjustPosition: true,
+                    props: {}
+                }
+            ]
+        });
+
+        return {
+            jsonData
+            // visualConfig
+        };
+    }
+
+    // data() {
+    //     return {
+    //         visualConfig,
+    //         jsonData:
+    //     };
+    // }
+});
+</script>
+
+<style lang="scss">
+html,
+body {
+    margin: 0;
+    padding: 0;
+}
+</style>
